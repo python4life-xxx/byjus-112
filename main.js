cfd5525e53b19f57232e40c11530e0421e50b05e
python4life-xxx <130096885+python4life-xxx@users.noreply.github.com>
2023-07-18 15:01:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+function setup(){
+    canvas = createCanvas(380, 380)
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.hide()
+    objectDetector = ml5.objectDetector('cocossd',  modelLoaded);
+    document.getElementById("status").innerHTML = "Estado: detectando objetos...";
+}
+
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage('dog_cat.jpg')
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+
+    if(status != ""){
+        r = random(255)
+        g = random(255)
+        b = random(255)
+
+        objectDetector.detect(video, gotResult)
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Estado: objetos detectados"
+            document.getElementById("number_of_objects").innerHTML = "Numero de objetos detectados; " + objects.length;
+            fill(r, g, b)
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " " + percent + " % ", objects[i].x, objects[i].y); 
+            noFill(); 
+            stroke(r, g, b); 
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Modelo cargado")
+    status = "true";
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error)
+    } else{
+        if(results){
+            console.log(results)
+            objects = results
+        }
+    }
+}
